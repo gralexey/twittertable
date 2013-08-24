7532d3a526f1aca077cc3da796b9bba555dd1eb5
Alexey Grabik <gralexey@mail.ru>
2013-08-24 13:52:22 +0400

    Initial Commit

---
+++ TweeterTable2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweeterTable2
+//
+//  Created by Alexey Grabik on 24.08.13.
+//  Copyright (c) 2013 Alexey Grabik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweeterTable2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TweeterTable2
+//
+//  Created by Alexey Grabik on 24.08.13.
+//  Copyright (c) 2013 Alexey Grabik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
